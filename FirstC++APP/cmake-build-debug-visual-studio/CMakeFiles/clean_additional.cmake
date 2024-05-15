6d16ca8f94f1e02c3205afca5b9011b14a99c181
xy-Summer-Sky <summer-xy2022@outlook.com>
2024-05-15 21:01:27 +0800

    init

---
+++ FirstC++APP/cmake-build-debug-visual-studio/CMakeFiles/clean_additional.cmake
@@ -0,0 +1,10 @@
+# Additional clean files
+cmake_minimum_required(VERSION 3.16)
+
+if("${CONFIG}" STREQUAL "" OR "${CONFIG}" STREQUAL "Debug")
+  file(REMOVE_RECURSE
+  "CMakeFiles\\FirstC__APP_autogen.dir\\AutogenUsed.txt"
+  "CMakeFiles\\FirstC__APP_autogen.dir\\ParseCache.txt"
+  "FirstC__APP_autogen"
+  )
+endif()
